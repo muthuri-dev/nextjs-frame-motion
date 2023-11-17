af3c14257cf85f841e379564973535dcd6219219
muthuriKE <muthurikennedy082@gmail.com>
2023-11-17 03:22:44 +0300

    Enter and Exit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 import React from 'react'
 import {motion} from 'framer-motion';
+import Textt from "@/components/Textt";
+import EnterExit from "@/components/EnterExit";
 
 export default function Home() {
     return (
-        <motion.h3 initial={{x:-1000}} animate={{x:[-1000,400,0]}} transition={{duration:3,delay:1}} whileHover={{scale:2, opacity:0.4}} className="text-white text-center">Animating text using framer motion</motion.h3>
+        <main>
+            <Textt/>
+            <EnterExit/>
+        </main>
     );
 }
---
+++ components/EnterExit.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from 'react'
+import {motion, AnimatePresence} from 'framer-motion';
+
+export default function EnterExit() {
+    return (
+        <motion.h3 ></motion.h3>
+    );
+}
