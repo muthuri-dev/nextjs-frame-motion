a59852cdddc74766a683aa4909184abe5220462d
muthuriKE <muthurikennedy082@gmail.com>
2023-11-17 11:56:51 +0300

    Exit and Enter animation

--- components/EnterExit.tsx
+++ components/EnterExit.tsx
@@ -3,7 +3,19 @@ import React from 'react'
 import {motion, AnimatePresence} from 'framer-motion';
 
 export default function EnterExit() {
+    const [show, setShow] = React.useState(true);
     return (
-        <motion.h3 ></motion.h3>
+        <div className="flex flex-col gap-[10px] items-center">
+            <h1 className="mt-[30px] text-white">Framer Motion Tutorial:Exit and Enter animation</h1>
+            <AnimatePresence>
+                {show && <motion.div className="w-[100px] h-[100px] bg-blue-500" initial={{opacity: 0, x: 0}}
+                                     animate={{opacity: 1, x: -100}} exit={{opacity: 0, x: -150}}
+                                     transition={{duration: 2, delay: 1}}>
+
+                </motion.div>}
+            </AnimatePresence>
+            <button onClick={() => setShow(!show)}
+                    className="text-white py-2 px-6 bg-blue-600 rounded-full">{show ? "Remove cube" : "Add Box"}</button>
+        </div>
     );
 }
